14270e9d56ef22b57e04352c1bcc61ce12f1f090
Amit Kumar <akumaramit7571@gmail.com>
2025-05-28 22:29:53 +0530

    Banner Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using ApnaBazaar.Models;
+using ApnaBazaar.Models.Db;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApnaBazaar.Controllers
@@ -7,14 +8,18 @@ namespace ApnaBazaar.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApnaBazaarContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApnaBazaarContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var banners = _context.Banners.ToList();
+            ViewData["banners"] = banners;
             return View();
         }
 
